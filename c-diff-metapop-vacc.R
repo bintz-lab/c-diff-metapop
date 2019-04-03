e1c0ee5252eba7bab4e9ed4753628a721456fdab
Jason Bintz <jasonbintz@gmail.com>
2019-04-03 17:25:49 -0400

    edited model equations

--- c-diff-metapop-vacc.R
+++ c-diff-metapop-vacc.R
@@ -20,43 +20,43 @@ cdiff_metapop_vacc <- function(t, state, parameters) {
          lambda2V0 <- betaC2V0*sumC2 + betaD2V0*sumD2
          lambda2V <- betaC2V*sumC2 + betaD2V*sumD2
          
-         dR1L <- b - (d1+alpha1L+(delta+d2)*N2/N1)*R1L + theta1L*S1L + xi1L*C1L + (1-qL)*delta*R2L
-         dS1L <-   - (d1+theta1L+lambda1L+(delta+d2)*N2/N1)*S1L + alpha1L*R1L + p1L*eps1L*D1L + (1-qL)*delta*S2L
-         dC1L <-   - (d1+xi1L+phi1L+(delta+d2)*N2/N1)*C1L + lambda1L*S1L + (1-qL)*delta*C2L
+         dR1L <- b - (d1+alpha1L+(delta+d2)*N2/N1)*R1L + theta1L*S1L + xi1L*C1L + (1-etaRL*qRL)*delta*R2L
+         dS1L <-   - (d1+theta1L+lambda1L+(delta+d2)*N2/N1)*S1L + alpha1L*R1L + p1L*eps1L*D1L + (1-etaSL*qSL)*delta*S2L
+         dC1L <-   - (d1+xi1L+phi1L+(delta+d2)*N2/N1)*C1L + lambda1L*S1L + (1-etaCL*qCL)*delta*C2L
          dD1L <-   - (d1+p1L*eps1L+(delta+d2)*N2/N1)*D1L + phi1L*C1L + delta*D2L
-           
-         dR1H <-  - (d1+alpha1H+(delta+d2)*N2/N1)*R1H + theta1H*S1H + xi1H*C1H + (1-qH)*delta*R2H
-         dS1H <-  - (d1+theta1H+lambda1H+(delta+d2)*N2/N1)*S1H + alpha1H*R1H + p1H*eps1H*D1H + (1-qH)*delta*S2H
-         dC1H <-  - (d1+xi1H+phi1H+(delta+d2)*N2/N1)*C1H + lambda1H*S1H + (1-qH)*delta*C2H
+         
+         dR1H <-  - (d1+alpha1H+(delta+d2)*N2/N1)*R1H + theta1H*S1H + xi1H*C1H + (1-etaRH*qRH)*delta*R2H
+         dS1H <-  - (d1+theta1H+lambda1H+(delta+d2)*N2/N1)*S1H + alpha1H*R1H + p1H*eps1H*D1H + (1-etaSH*qSH)*delta*S2H
+         dC1H <-  - (d1+xi1H+phi1H+(delta+d2)*N2/N1)*C1H + lambda1H*S1H + (1-etaCH*qCH)*delta*C2H
          dD1H <-  - (d1+p1H*eps1H+(delta+d2)*N2/N1)*D1H + phi1H*C1H + delta*D2H
-          
-         dR1V0 <- - (d1+alpha1V0+(delta+d2)*N2/N1+nu)*R1V0 + theta1V0*S1V0 + xi1V0*C1V0 + delta*(R2V0+qL*R2L+qH*R2H)
-         dS1V0 <- - (d1+theta1V0+lambda1V0+(delta+d2)*N2/N1+nu)*S1V0 + alpha1V0*R1V0 + p1V0*eps1V0*D1V0 + delta*(S2V0+qL*S2L+qH*S2H)
-         dC1V0 <- - (d1+xi1V0+phi1V0+(delta+d2)*N2/N1+nu)*C1V0 + lambda1V0*S1V0 + delta*(C2V0+qL*C2L+qH*C2H)
+         
+         dR1V0 <- - (d1+alpha1V0+(delta+d2)*N2/N1+nuR)*R1V0 + theta1V0*S1V0 + xi1V0*C1V0 + delta*(R2V0+etaRL*qRL*R2L+etaRH*qRH*R2H)
+         dS1V0 <- - (d1+theta1V0+lambda1V0+(delta+d2)*N2/N1+nuS)*S1V0 + alpha1V0*R1V0 + p1V0*eps1V0*D1V0 + delta*(S2V0+etaSL*qSL*S2L+etaSH*qSH*S2H)
+         dC1V0 <- - (d1+xi1V0+phi1V0+(delta+d2)*N2/N1+nuC)*C1V0 + lambda1V0*S1V0 + delta*(C2V0+etaCL*qCL*C2L+etaCH*qCH*C2H)
          dD1V0 <- - (d1+p1V0*eps1V0+(delta+d2)*N2/N1)*D1V0 + phi1V0*C1V0 + delta*D2V0
          
-         dR1V <-  - (d1+alpha1V+(delta+d2)*N2/N1)*R1V + theta1V*S1V + xi1V*C1V + delta*R2V + nu*R1V0
-         dS1V <-  - (d1+theta1V+lambda1V+(delta+d2)*N2/N1)*S1V + alpha1V*R1V + delta*S2V + nu*S1V0
-         dC1V <-  - (d1+xi1V+(delta+d2)*N2/N1)*C1V + lambda1V*S1V + delta*C2V + nu*C1V0
+         dR1V <-  - (d1+alpha1V+(delta+d2)*N2/N1)*R1V + theta1V*S1V + xi1V*C1V + delta*R2V + nuR*R1V0
+         dS1V <-  - (d1+theta1V+lambda1V+(delta+d2)*N2/N1)*S1V + alpha1V*R1V + delta*S2V + nuS*S1V0
+         dC1V <-  - (d1+xi1V+(delta+d2)*N2/N1)*C1V + lambda1V*S1V + delta*C2V + nuC*C1V0
          
-         dR2L <-   - (d2+alpha2L+delta)*R2L + theta2L*S2L + xi2L*C2L + (1-r)*(delta+d2)*N2/N1*R1L
-         dS2L <-   - (d2+theta2L+lambda2L+delta)*S2L + alpha2L*R2L + p2L*eps2L*D2L + (1-r)*(delta+d2)*N2/N1*S1L
-         dC2L <-   - (d2+xi2L+phi2L+delta)*C2L + lambda2L*S2L + (1-r)*(delta+d2)*N2/N1*C1L
-         dD2L <-   - (d2+p2L*eps2L+delta)*D2L + phi2L*C2L + (1-r)*(delta+d2)*N2/N1*D1L
+         dR2L <-   - (d2+alpha2L+delta)*R2L + theta2L*S2L + xi2L*C2L + (1-rR)*(delta+d2)*N2/N1*R1L
+         dS2L <-   - (d2+theta2L+lambda2L+delta)*S2L + alpha2L*R2L + p2L*eps2L*D2L + (1-rS)*(delta+d2)*N2/N1*S1L
+         dC2L <-   - (d2+xi2L+phi2L+delta)*C2L + lambda2L*S2L + (1-rC)*(delta+d2)*N2/N1*C1L
+         dD2L <-   - (d2+p2L*eps2L+delta)*D2L + phi2L*C2L + (1-rD)*(delta+d2)*N2/N1*D1L
          
-         dR2H <-  - (d2+alpha2H+delta)*R2H + theta2H*S2H + xi2H*C2H + (delta+d2)*N2/N1*(r*R1L+R1H)
-         dS2H <-  - (d2+theta2H+lambda2H+delta)*S2H + alpha2H*R2H + p2H*eps2H*D2H + (delta+d2)*N2/N1*(r*S1L+S1H)
-         dC2H <-  - (d2+xi2H+phi2H+delta)*C2H + lambda2H*S2H + (delta+d2)*N2/N1*(r*C1L+C1H)
-         dD2H <-  - (d2+p2H*eps2H+delta)*D2H + phi2H*C2H + (delta+d2)*N2/N1*(r*D1L+D1H)
+         dR2H <-  - (d2+alpha2H+delta)*R2H + theta2H*S2H + xi2H*C2H + (delta+d2)*N2/N1*(rR*R1L+R1H)
+         dS2H <-  - (d2+theta2H+lambda2H+delta)*S2H + alpha2H*R2H + p2H*eps2H*D2H + (delta+d2)*N2/N1*(rS*S1L+S1H)
+         dC2H <-  - (d2+xi2H+phi2H+delta)*C2H + lambda2H*S2H + (delta+d2)*N2/N1*(rC*C1L+C1H)
+         dD2H <-  - (d2+p2H*eps2H+delta)*D2H + phi2H*C2H + (delta+d2)*N2/N1*(rD*D1L+D1H)
          
-         dR2V0 <- - (d2+alpha2V0+delta+nu)*R2V0 + theta2V0*S2V0 + xi2V0*C2V0 + (delta+d2)*N2/N1*R1V0
-         dS2V0 <- - (d2+theta2V0+lambda2V0+delta+nu)*S2V0 + alpha2V0*R2V0 + p2V0*eps2V0*D2V0 + (delta+d2)*N2/N1*S1V0
-         dC2V0 <- - (d2+xi2V0+phi2V0+delta+nu)*C2V0 + lambda2V0*S2V0 + (delta+d2)*N2/N1*C1V0
+         dR2V0 <- - (d2+alpha2V0+delta+nuR)*R2V0 + theta2V0*S2V0 + xi2V0*C2V0 + (delta+d2)*N2/N1*R1V0
+         dS2V0 <- - (d2+theta2V0+lambda2V0+delta+nuS)*S2V0 + alpha2V0*R2V0 + p2V0*eps2V0*D2V0 + (delta+d2)*N2/N1*S1V0
+         dC2V0 <- - (d2+xi2V0+phi2V0+delta+nuC)*C2V0 + lambda2V0*S2V0 + (delta+d2)*N2/N1*C1V0
          dD2V0 <- - (d2+p2V0*eps2V0+delta)*D2V0 + phi2V0*C2V0 + (delta+d2)*N2/N1*D1V0
          
-         dR2V <-  - (d2+alpha2V+delta)*R2V + theta2V*S2V + xi2V*C2V + nu*R2V0 + (delta+d2)*N2/N1*R1V
-         dS2V <-  - (d2+theta2V+lambda2V+delta)*S2V + alpha2V*R2V + nu*S2V0 + (delta+d2)*N2/N1*S1V
-         dC2V <-  - (d2+xi2V+delta)*C2V + lambda2V*S2V + nu*C2V0 + (delta+d2)*N2/N1*C1V
+         dR2V <-  - (d2+alpha2V+delta)*R2V + theta2V*S2V + xi2V*C2V + nuR*R2V0 + (delta+d2)*N2/N1*R1V
+         dS2V <-  - (d2+theta2V+lambda2V+delta)*S2V + alpha2V*R2V + nuS*S2V0 + (delta+d2)*N2/N1*S1V
+         dC2V <-  - (d2+xi2V+delta)*C2V + lambda2V*S2V + nuC*C2V0 + (delta+d2)*N2/N1*C1V
          
          list(c(dR1L, dS1L, dC1L, dD1L, dR1H, dS1H, dC1H, dD1H, dR1V0, dS1V0, dC1V0, dD1V0, dR1V, dS1V, dC1V, dR2L, dS2L, dC2L, dD2L, dR2H, dS2H, dC2H, dD2H, dR2V0, dS2V0, dC2V0, dD2V0, dR2V, dS2V, dC2V))
        })
@@ -96,7 +96,11 @@ parameters <- c(N1 = N1, N2 = N2, d1 = d1, d2 = d2, b = b,
                 alpha2V = 0.5, theta2V = 0.0033, xi2V = 0.0165, 
                 betaC2V = 0.007, betaD2V = 0.007,
                 
-                qL = 0.25, qH = 0.25, r = 0.25, nu = 2/365, delta = 0.135)
+                etaRL = 1, qRL = 0.25, etaSL = 1, qSL = 0.25, etaCL = 1, qCL = 0.25,
+                etaRH = 1, qRH = 0.25, etaSH = 1, qSH = 0.25, etaCH = 1, qCH = 0.25,
+                rR = 0.25, rS = 0.25, rC = 0.25, rD = 0.25,
+                nuR = 2/365, nuS = 2/365, nuC = 2/365,
+                delta = 0.135)
 
 state <- c(R1L = 0.63*N1, S1L = 0.18*N1, C1L = 0.063*N1, D1L = 0.027*N1,
            R1H = 0.07*N1, S1H = 0.02*N1, C1H = 0.007*N1, D1H = 0.003*N1,
